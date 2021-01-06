3628e4b14fef32ab39294da29bcf4805efce3cd4
Théo Brouillé <theo.brouille3@gmail.com>
2021-01-06 16:00:21 +0100

    Added a title and a plus button to the navigation bar.

--- QuickNote/Scenes/HomeViewController.swift
+++ QuickNote/Scenes/HomeViewController.swift
@@ -10,5 +10,15 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "QuickNote"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let rightButton = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = rightButton
     }
 }
